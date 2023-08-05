71338e8a303f7b2442f4c46df318c0618e934b2f
Matilde Rosa <77325729+matzro@users.noreply.github.com>
2023-08-05 15:51:03 +0100

    Added winner

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ function changeDices(){
     var player2 = Math.floor(Math.random() * (6 - 1) + 1);
     document.getElementById("player1").src = "images/dice" + player1 + ".png";
     document.getElementById("player2").src = "images/dice" + player2 + ".png";
+
+    if(player1 > player2){
+        document.querySelector("h1").innerHTML = "Player 1 wins";
+    }
+    else{
+        document.querySelector("h1").innerHTML = "Player 2 wins";
+    }
 }
+
+
 
